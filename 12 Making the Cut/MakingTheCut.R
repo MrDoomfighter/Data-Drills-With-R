b5382cddfa3570b98d7acfd0a29f26ee1426dc1c
doomfighter <daniel-koenigs@gmx.de>
2026-05-28 23:36:03 +0200

    Solution for Data Drill 12 "Making the Cut" added

---
+++ 12 Making the Cut/MakingTheCut.R
@@ -0,0 +1,31 @@
+# load libraries
+library(readr)
+library(dplyr)
+
+# load data
+marathonData = read_csv("./12 Making the Cut/marathon-data.csv")
+
+# aggregate data
+marathonData |>
+  mutate(
+    finishBand = cut(
+      final |> as.numeric() / 3600,
+      breaks = c(0, 3, 3.5, 4, 4.5, 5, 5.5, 6, Inf),
+      labels = c(
+        'Sub 3:00',
+        '3:00 - 3:30',
+        '3:30 - 4:00',
+        '4:00 - 4:30',
+        '4:30 - 5:00',
+        '5:00 - 5:30',
+        '5:30 - 6:00',
+        '6:00+'
+      ),
+      right = FALSE
+    )
+  ) |>
+  summarise(
+    runners = n(),
+    .by = finishBand
+  ) |>
+  mutate(rate = floor(runners / sum(runners) * 100))
